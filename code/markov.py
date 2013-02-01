d74bfb12710de6e74e6afb00bca93b4f370011bc
Dylan Fried <dylan.fried@gmail.com>
2013-01-31 19:27:10 -0800

    changed format, again...

--- code/markov.py
+++ code/markov.py
@@ -7,23 +7,22 @@ class Markov:
    #  - order: The max order generated
    #  - primary_key:  This is what the Markov transitions from and to (ie parts of speech or words),
    #    given as the index within the line (10 for part of speech, 11 for word)
-   #  - secondary_key: This is the key used for bucketing (ie part of speech within the word Markov)
-   #    given as the index within the line (10 for part of speech, 11 for word)
-   def __init__(self, lines, order, primary_key, secondary_key):
+   def __init__(self, lines, order, primary_key):
       self.lines = lines
       self.order = order
       self.primary_key = primary_key
-      self.secondary_key = secondary_key
       self.lines_so_far = []
    
    # The initialize method initializes the markov chain
-   # This takes care of the meat of the operation
    def initialize():
+      
       
    # This method generates the single next element in the markov chain. This element is added to the lines_so_far and
    # returned as well. This method will also incorporate the backoff logic.
    # -order: The order of the markov chain for this generation
-   # -emotion: A dictionary of the target emotional levels. ex:
-   #   {"anger" : 3.0, "fear" : 2.0, "joy": None, "sadness": None, "freq": 1.0}
-   # -secondary_key: This key contains the part of speech as a string.
-   def generateNext(order, emotion, secondary_key=None):
+   # -filters: This is a list of filters for the transition (ie affect level or part of speech). It is given
+   #  as a list of dictionaries with the index into the line (following the brute.txt format) for what we're 
+   #  filtering on and the filter itself (ie what we're matching). ex:
+   #   [{"index": 10, "filter": "noun", "type": "text_match"},
+   #    {"index": 8, "filter": 3.0, "type": "threshold"}]
+   def generateNext(order, filters):
