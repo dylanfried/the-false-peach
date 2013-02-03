c4a0576bce665f75c2d9bddcac1c6241f3845225
Dylan Fried <dylan.fried@gmail.com>
2013-02-02 17:22:30 -0800

    added rough draft of two xml->continuous conversions

--- code/xml_client.py
+++ code/xml_client.py
@@ -14,6 +14,115 @@ sys.path.append("code/")
 #  - The trial config file
 #  - The trigger config file
 
+# Helper function for pulling lines of the play out based
+# on XML constraints in a trial
+def get_lines(data, xml_constraints):
+   trial_data = []
+   constraints = []
+   selections = xml_constraints.find("selections")
+
+   if selections:
+      for selection in selections.findAll("selection"):
+
+         acts = selection.find("acts")
+         if acts: 
+            acts = acts.string
+            acts = expand(acts.split(","))
+         else: acts=[]
+
+         scenes = selection.find("scenes")
+         if scenes: 
+            scenes = scenes.string
+            scenes = expand(scenes.split(","))
+         else: scenes=[]
+
+         lines = selection.find("lines")
+         if lines: 
+            lines = lines.string
+            lines = expand(lines.split(","))
+         else: lines=[]
+      
+         characters = selection.find("characters")
+         if characters: 
+            characters = characters.string
+            characters = characters.split(",")
+            characters = [c.strip() for c in characters]
+            characters = [re.sub(" ","_",c.upper()) for c in characters]
+         else: characters=[]
+
+         pos = selection.find("pos")
+         if pos:
+            pos = pos.string
+            pos = pos.split(",")
+            pos = [c.strip() for c in pos]
+            pos = [c.upper() for c in pos]
+         else: pos=[]
+
+         constraints.append({"acts":acts,"scenes":scenes,"lines":lines,"characters":characters,"pos":pos})
+
+   else:
+      # Only a single constraint
+      acts = xml_constraints.find("acts")
+      if acts:
+         acts = acts.string
+         acts = expand(acts.split(","))
+      else: acts=[]
+
+      scenes = xml_constraints.find("scenes")
+      if scenes:
+         scenes = scenes.string
+         scenes = expand(scenes.split(","))
+      else: scenes=[]
+
+      lines = xml_constraints.find("lines")
+      if lines:
+         lines = lines.string
+         lines = expand(lines.split(","))
+      else: lines=[]
+
+      characters = xml_constraints.find("characters")
+      if characters:
+         characters = characters.string
+         characters = characters.split(",")
+         characters = [c.strip() for c in characters]
+         characters = [re.sub(" ","_",c.upper()) for c in characters]
+      else: characters=[]
+
+      constraints.append({"acts":acts,"scenes":scenes,"lines":lines,"characters":characters})
+
+   for m in range(len(data)):
+
+      keep = []
+
+      for c in constraints:
+
+         if c["acts"] and (not data[m][0] in c["acts"]): 
+            keep.append(0)
+            continue
+     
+         if c["scenes"] and (not data[m][1] in c["scenes"]): 
+            keep.append(0)
+            continue
+
+         if c["lines"] and (not data[m][2] in c["lines"]): 
+            keep.append(0)
+            continue
+
+         if c["characters"] and (not data[m][4].upper() in c["characters"]): 
+            keep.append(0)
+            continue
+
+         if 'pos' in c and c['pos']:
+            if data[m][nmax-2]=="SPEAKER" or data[m][nmax-2].upper() in c['pos'] or data[m][nmax-1]=="NEWLINE" and not data[m][nmax-1] in [")","("]: keep.append(1)
+            else: keep.append(0)
+         else: keep.append(1)
+
+      if keep and max(keep)==0: continue
+
+      trial_data.append(data[m])
+   return trial_data
+
+
 # Grab the config files
 if len(sys.argv)>2:
    trialconfigfile = sys.argv[2]
@@ -39,6 +148,7 @@ for trial in bs.findAll(recursive=False):
    # Match the strategy name to a set of parameters to pass on to
    # the generator
    # We're going to fill in these variables:
+   trial_length = 0
    POS_training_text = []
    POS_order_ramp = []
    POS_emotion_ramp = []
@@ -46,15 +156,41 @@ for trial in bs.findAll(recursive=False):
    word_order_ramp = []
    word_emotion_ramp = []
    
+   # Grab the data file
+   datafile = trial.find("file")
+   if datafile:
+      datafile = datafile.string
+   else: continue
+   
+   # Grab the data from the data file
+   data = open(datafile).readlines()
+   data = [d.strip() for d in data]
+   data = [d.split(" ") for d in data]
+   
    if trial.name == "sm_filter":
-      
+      print "sm_filter not yet implemented"
+      continue
+      #lines = get_lines(data, trial.find('train'))
    elif: trial.name == "filter":
-      
+      print "filter not yet implemented"
+      continue
    elif: trial.name == "skip":
-      
+      print "skip not yet implemented"
+      continue
    elif: trial.name == "mirror":
-      
+      POS_training_text = get_lines(data, trial.find('train'))
+      POS_order_ramp.append({"order":10, "word_number": 1})
+      POS_emotion_ramp = []
+      word_training_text = get_lines(data, trial.find('generate'))
+      word_order_ramp.append({"order":0, "word_number": 1})
+      word_emotion_ramp = []
    elif: trial.name == "markov":
+      POS_training_text = get_lines(data, trial.find('train'))
+      POS_order_ramp.append({"order":0, "word_number": 1})
+      POS_emotion_ramp = []
+      word_training_text = get_lines(data, trial.find('train'))
+      word_order_ramp.append({"order":trial.find("generate").find("k"), "word_number": 1})
+      word_emotion_ramp = []
       
    else:
       print "Bad trial type ", trial.name
