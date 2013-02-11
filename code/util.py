8422d935657097007d24e18d09f2bd0373b7bf08
Dylan Fried <dylan.fried@gmail.com>
2013-02-11 13:14:18 -0800

    moved helper functions to util

--- code/util.py
+++ code/util.py
@@ -1,3 +1,4 @@
+import re
 # This file houses useful functions that may be used in many
 # places
 
@@ -22,3 +23,139 @@ def interpolate(x,y,m):
       yright = y[right]+0.0
       val.append( yright*(xleft-i)/(xleft-xright)+yleft*(i-xright)/(xleft-xright) )
    return val
+   
+
+# Helper function for expanding number ranges (eg, 20-24)
+def expand(things):
+   out = []
+   for t in things:
+      if not "-" in t: out.append(t)
+      else:
+         start = int(re.sub("([0-9]+)\-.*","\\1",t))   
+         end = int(re.sub(".*\-([0-9]+)","\\1",t))   
+         out += [str(r) for r in range(start,end+1)]
+   return out
+
+# Helper function for pulling lines of the play out based
+# on XML constraints in a trial
+def get_lines(data, xml_constraints):
+   trial_data = []
+   constraints = []
+   selections = xml_constraints.find("selections")
+
+   if selections:
+      for selection in selections.findAll("selection"):
+
+         acts = selection.find("acts")
+         if acts: 
+            acts = acts.string
+            acts = expand(acts.split(","))
+         else: acts=[]
+
+         scenes = selection.find("scenes")
+         if scenes: 
+            scenes = scenes.string
+            scenes = expand(scenes.split(","))
+         else: scenes=[]
+
+         lines = selection.find("lines")
+         if lines: 
+            lines = lines.string
+            lines = expand(lines.split(","))
+         else: lines=[]
+         
+         words = selection.find("words")
+         if words: 
+            words = words.string
+            words = expand(words.split(","))
+         else: words=[]
+      
+         characters = selection.find("characters")
+         if characters: 
+            characters = characters.string
+            characters = characters.split(",")
+            characters = [c.strip() for c in characters]
+            characters = [re.sub(" ","_",c.upper()) for c in characters]
+         else: characters=[]
+
+         pos = selection.find("pos")
+         if pos:
+            pos = pos.string
+            pos = pos.split(",")
+            pos = [c.strip() for c in pos]
+            pos = [c.upper() for c in pos]
+         else: pos=[]
+
+         constraints.append({"acts":acts,"scenes":scenes,"lines":lines,"words":words,"characters":characters,"pos":pos})
+
+   else:
+      # Only a single constraint
+      acts = xml_constraints.find("acts")
+      if acts:
+         acts = acts.string
+         acts = expand(acts.split(","))
+      else: acts=[]
+
+      scenes = xml_constraints.find("scenes")
+      if scenes:
+         scenes = scenes.string
+         scenes = expand(scenes.split(","))
+      else: scenes=[]
+
+      lines = xml_constraints.find("lines")
+      if lines:
+         lines = lines.string
+         lines = expand(lines.split(","))
+      else: lines=[]
+      
+      words = xml_constraints.find("words")
+      if words: 
+         words = words.string
+         words = expand(words.split(","))
+      else: words=[]
+
+      characters = xml_constraints.find("characters")
+      if characters:
+         characters = characters.string
+         characters = characters.split(",")
+         characters = [c.strip() for c in characters]
+         characters = [re.sub(" ","_",c.upper()) for c in characters]
+      else: characters=[]
+
+      constraints.append({"acts":acts,"scenes":scenes,"lines":lines,"words":words,"characters":characters})
+
+   for m in range(len(data)):
+
+      keep = []
+
+      for c in constraints:
+
+         if c["acts"] and (not data[m][0] in c["acts"]): 
+            keep.append(0)
+            continue
+     
+         if c["scenes"] and (not data[m][1] in c["scenes"]): 
+            keep.append(0)
+            continue
+
+         if c["lines"] and (not data[m][2] in c["lines"]): 
+            keep.append(0)
+            continue
+            
+         if c["words"] and (not data[m][3] in c["words"]): 
+            keep.append(0)
+            continue
+
+         if c["characters"] and (not data[m][4].upper() in c["characters"]): 
+            keep.append(0)
+            continue
+
+         if 'pos' in c and c['pos']:
+            if data[m][-2]=="SPEAKER" or data[m][-2].upper() in c['pos'] or data[m][-1]=="NEWLINE" and not data[m][-1] in [")","("]: keep.append(1)
+            else: keep.append(0)
+         else: keep.append(1)
+
+      if keep and max(keep)==0: continue
+
+      trial_data.append(data[m])
+   return trial_data
--- code/xml_client.py
+++ code/xml_client.py
@@ -19,146 +19,6 @@ from loosey_client import LooseyClient
 #  - The trial config file
 #  - The trigger config file
 
-# Helper function for pulling lines of the play out based
-# on XML constraints in a trial
-def get_lines(data, xml_constraints):
-   trial_data = []
-   constraints = []
-   selections = xml_constraints.find("selections")
-
-   if selections:
-      for selection in selections.findAll("selection"):
-
-         acts = selection.find("acts")
-         if acts: 
-            acts = acts.string
-            acts = expand(acts.split(","))
-         else: acts=[]
-
-         scenes = selection.find("scenes")
-         if scenes: 
-            scenes = scenes.string
-            scenes = expand(scenes.split(","))
-         else: scenes=[]
-
-         lines = selection.find("lines")
-         if lines: 
-            lines = lines.string
-            lines = expand(lines.split(","))
-         else: lines=[]
-         
-         words = selection.find("words")
-         if words: 
-            words = words.string
-            words = expand(words.split(","))
-         else: words=[]
-      
-         characters = selection.find("characters")
-         if characters: 
-            characters = characters.string
-            characters = characters.split(",")
-            characters = [c.strip() for c in characters]
-            characters = [re.sub(" ","_",c.upper()) for c in characters]
-         else: characters=[]
-
-         pos = selection.find("pos")
-         if pos:
-            pos = pos.string
-            pos = pos.split(",")
-            pos = [c.strip() for c in pos]
-            pos = [c.upper() for c in pos]
-         else: pos=[]
-
-         constraints.append({"acts":acts,"scenes":scenes,"lines":lines,"words":words,"characters":characters,"pos":pos})
-
-   else:
-      # Only a single constraint
-      acts = xml_constraints.find("acts")
-      if acts:
-         acts = acts.string
-         acts = expand(acts.split(","))
-      else: acts=[]
-
-      scenes = xml_constraints.find("scenes")
-      if scenes:
-         scenes = scenes.string
-         scenes = expand(scenes.split(","))
-      else: scenes=[]
-
-      lines = xml_constraints.find("lines")
-      if lines:
-         lines = lines.string
-         lines = expand(lines.split(","))
-      else: lines=[]
-      
-      words = xml_constraints.find("words")
-      if words: 
-         words = words.string
-         words = expand(words.split(","))
-      else: words=[]
-
-      characters = xml_constraints.find("characters")
-      if characters:
-         characters = characters.string
-         characters = characters.split(",")
-         characters = [c.strip() for c in characters]
-         characters = [re.sub(" ","_",c.upper()) for c in characters]
-      else: characters=[]
-
-      constraints.append({"acts":acts,"scenes":scenes,"lines":lines,"words":words,"characters":characters})
-
-   for m in range(len(data)):
-
-      keep = []
-
-      for c in constraints:
-
-         if c["acts"] and (not data[m][0] in c["acts"]): 
-            keep.append(0)
-            continue
-     
-         if c["scenes"] and (not data[m][1] in c["scenes"]): 
-            keep.append(0)
-            continue
-
-         if c["lines"] and (not data[m][2] in c["lines"]): 
-            keep.append(0)
-            continue
-            
-         if c["words"] and (not data[m][3] in c["words"]): 
-            keep.append(0)
-            continue
-
-         if c["characters"] and (not data[m][4].upper() in c["characters"]): 
-            keep.append(0)
-            continue
-
-         if 'pos' in c and c['pos']:
-            if data[m][-2]=="SPEAKER" or data[m][-2].upper() in c['pos'] or data[m][-1]=="NEWLINE" and not data[m][-1] in [")","("]: keep.append(1)
-            else: keep.append(0)
-         else: keep.append(1)
-
-      if keep and max(keep)==0: continue
-
-      trial_data.append(data[m])
-   return trial_data
-
-# Helper function
-def expand(things):
-
-   out = []
-
-   for t in things:
-
-      if not "-" in t: out.append(t)
-      else:
-         start = int(re.sub("([0-9]+)\-.*","\\1",t))   
-         end = int(re.sub(".*\-([0-9]+)","\\1",t))   
-         out += [str(r) for r in range(start,end+1)]
-
-   return out
-
-
 # Grab the config files
 if len(sys.argv)>2:
    trialconfigfile = sys.argv[2]
@@ -237,7 +97,7 @@ for scene in bs.findAll("scene"):
       elif trial.name == "filter":
          print "Filter"
          scene_lines.append("=================== CHUNK 1 " + trialname + " ================")
-         trial_data = get_lines(data, trial.find("train"))
+         trial_data = util.get_lines(data, trial.find("train"))
          #get the pattern
          pattern = trial.find("generate").find("pattern")
          if pattern: pattern = pattern.string
@@ -337,21 +197,21 @@ for scene in bs.findAll("scene"):
          continue
       elif trial.name == "mirror":
          print "Mirror"
-         POS_training_text = get_lines(data, trial.find('generate'))
+         POS_training_text = util.get_lines(data, trial.find('generate'))
          #print "POS_training Text", POS_training_text
          POS_order_ramp.append({"order":10, "word_number": 1})
          POS_emotion_ramp = []
-         word_training_text = get_lines(data, trial.find('train'))
+         word_training_text = util.get_lines(data, trial.find('train'))
          word_order_ramp.append({"order":0, "word_number": 1})
          word_emotion_ramp = []
          # Trial length should just be length of POS training text
          trial_length = len(POS_training_text)
       elif trial.name == "markov":
          print "Markov"
-         POS_training_text = get_lines(data, trial.find('train'))
+         POS_training_text = util.get_lines(data, trial.find('train'))
          POS_order_ramp.append({"order":-1, "word_number": 1})
          POS_emotion_ramp = []
-         word_training_text = get_lines(data, trial.find('train'))
+         word_training_text = util.get_lines(data, trial.find('train'))
          word_order_ramp.append({"order":int(trial.find("generate").find("k").string), "word_number": 1})
          word_emotion_ramp = []
          trial_length = int(trial.find("generate").find("length").string)
