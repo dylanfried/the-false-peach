43eb4f713424382d67a3bc6cb250f1dd19eff785
Paul Calley <pcalley@ucsc.edu>
2013-02-11 20:53:48 -0800

    Comments for pinnning table

--- code/pinning_table.py
+++ code/pinning_table.py
@@ -1,6 +1,36 @@
-class PinningTable:
+import os
+from bs4 import BeautifulSoup
+
+# Class for defining the Pinning Table that specifies constriants on which
+# styles can occur for a scene. Also can pin which styles can occur together.
+# Methods:
+# - constructor:
+# - generated_style: outputs an array of length four of styles given a scene. 
+class pinningTable:
+   
+   # Constructor has attributes.
+   # - table: Four dimensional dictionary object. 
+   #   {"scene_name": 
+   #     {"video_name": 
+   #         {"sound_name":
+   #             {"lights_name1": 1},
+   #              {"lights_name2": 1}
+   #            }
+   #      },
+   #    "scene_name": 
+   #      {"video_name": 
+   #         {"sound_name":
+   #             {"lights_name1": 1},
+   #              {"lights_name2": 1}
+   #          }
+   #       }
+   #   }
    def __init__(self, pinning_file):
-      pass
+      self.table = BeautifulSoup(open(pinning_file).read()).find("table")
+
    # Given scene, return a style
    def generate_style(self, scene):
-      pass
+      for include in includes:
+         bs_scene = include.find("scene")
+         if bs_scene == scene:
+            pass
