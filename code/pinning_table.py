4fbddd4f3f99b6e0f560b9874b5bb0eaf8a62c12
Dylan Fried <dylan.fried@gmail.com>
2013-02-12 12:31:40 -0800

    first at least semi-working draft of pinnings

--- code/pinning_table.py
+++ code/pinning_table.py
@@ -1,12 +1,14 @@
 import os
 from bs4 import BeautifulSoup
+import sys
+import re
 
 # Class for defining the Pinning Table that specifies constriants on which
 # styles can occur for a scene. Also can pin which styles can occur together.
 # Methods:
 # - constructor:
 # - generated_style: outputs an array of length four of styles given a scene. 
-class pinningTable:
+class PinningTable:
    
    # Constructor has attributes.
    # - table: Four dimensional dictionary object. 
@@ -26,15 +28,107 @@ class pinningTable:
    #       }
    #   }
    def __init__(self, pinning_file):
-      self.table = BeautifulSoup(open(pinning_file).read()).find("table")
+      self.table = {}
       # Grab scene files from directory
+      scenes = os.listdir("config/SHOW/")
+      scenes = [re.sub("^(.*)\.xml$","\\1",s) for s in scenes if re.match("^.*\.xml$",s)]
       # Keep list of possible styles
+      video_styles = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17"]
+      sound_styles = ["TTS.aloud","TTS.space.word","TTS.space.character","TTS.mute","TTS.inear","TTS.mute.music.emotion","TTS.music.emotion","TTS.music.phoneme","TTS.rogui","VOICE.music","VOICE.rogui"]
+      lights_styles = ["skot","theatre","emotions","play","word","ghost","monemo","lightout","lightshow","closet"]
       # Initialize and populate dictionary
+      for scene in scenes:
+         self.table[scene] = {}
+         for video_style in video_styles:
+            self.table[scene][video_style] = {}
+            for sound_style in sound_styles:
+               self.table[scene][video_style][sound_style] = {}
+               for lights_style in lights_styles:
+                  self.table[scene][video_style][sound_style][lights_style] = True
+      # Go through our pinnings and excludes
+      table = BeautifulSoup(open(pinning_file).read()).find("table")
+      for constraint in table.findAll(["pinned","excluded"]):
+         styles = constraint.findAll(["scene","sound","video","lights"])
+         # Construct/populate a dictionary to represent the 
+         # constraint that we're on
+         to_delete = {"scene":scenes[:],"sound":sound_styles[:],"video":video_styles[:],"lights":lights_styles[:]}
+         for s in styles:
+            if s.name in to_delete:
+               #if constraint.name == "pinned":
+               #   to_delete[s.name] = [style for style in to_delete[s.name] if style != s.string]
+               #elif constraint.name == "excluded":
+               to_delete[s.name] = [s.string]
+               #else:
+               #   print "Unacceptable type:",constraint.name
+            else:
+               print "Unacceptable type:", s.name
+         # Now loop through the deletion keys and get rid of that part of the table
+         if constraint.name == "excluded":
+            for scene in to_delete["scene"]:
+               for video_style in to_delete["video"]:
+                  for sound_style in to_delete["sound"]:
+                     for lights_style in to_delete["lights"]:
+                        self.table[scene][video_style][sound_style][lights_style] = False
+         else:
+            for scene in self.table.keys():
+               if len(to_delete["scene"]) == 1 and to_delete["scene"][0] == scene:
+                  matched = 1
+               else:
+                  matched = 0
+               for video_style in self.table[scene].keys():
+                  if len(to_delete["video"]) == 1 and to_delete["video"][0] == video_style:
+                     video_matched = matched + 1
+                  else:
+                     video_matched = matched
+                  for sound_style in self.table[scene][video_style].keys():
+                     if len(to_delete["sound"]) == 1 and to_delete["sound"][0] == sound_style:
+                        sound_matched = video_matched + 1
+                     else:
+                        sound_matched = video_matched
+                     for lights_style in self.table[scene][video_style][sound_style].keys():
+                        if len(to_delete["lights"]) == 1 and to_delete["lights"][0] == lights_style:
+                           lights_matched = sound_matched + 1
+                        else:
+                           lights_matched = sound_matched
+                        #print scene, video_style, sound_style, lights_style, lights_matched
+                        if lights_matched > 0 and lights_matched != len(styles):
+                           self.table[scene][video_style][sound_style][lights_style] = False
+                        
+         #print self.table["2bMirror"]["11"]
+      # Now that we've done all of our setting to false, let's delete everything
+      # that is False and all empty branches
+      for scene in self.table.keys():
+         possible_styles = 0
+         for video_style in self.table[scene].keys():
+            for sound_style in self.table[scene][video_style].keys():
+               for lights_style in self.table[scene][video_style][sound_style].keys():
+                  if not self.table[scene][video_style][sound_style][lights_style]:
+                     del self.table[scene][video_style][sound_style][lights_style]
+                  else:
+                     possible_styles += 1
+               if not self.table[scene][video_style][sound_style]:
+                  del self.table[scene][video_style][sound_style]
+            if not self.table[scene][video_style]:
+               del self.table[scene][video_style]
+         print "Scene",scene,"has",str(possible_styles),"potential styles"
+         if not self.table[scene]:
+            del self.table[scene]
 
    # Given scene, return a style
    def generate_style(self, scene):
+      if scene not in self.table:
+         print "NO STYLES FOR SCENE",scene
+         return ""
       video = random.sample(self.table[scene].keys(), 1)[0]
       sound = random.sample(self.table[scene][video].keys(), 1)[0]
       lights = random.sample(self.table[scene][video][sound].keys(), 1)[0]
       
       return video + "_" + sound + "_0_" + lights
+      
+def main(argv):
+   # Grab the pinning files
+   p = PinningTable("config/pinning.xml")
+   print p.table["2bMirror"]["5"]
+   
+if __name__ == "__main__":
+   main(sys.argv)
