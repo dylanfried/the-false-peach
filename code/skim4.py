7587551b359bc8316bb5269d06f1fdceb1ba446b
Paul Calley <pcalley@ucsc.edu>
2013-02-18 17:40:44 -0800

    Added comments

--- code/skim4.py
+++ code/skim4.py
@@ -1,15 +1,16 @@
-##### Line 
-
+##### Makes a new brute.txt called out_test.txt, which can be checked and
+##### renamed brute.txt.
 import re
 import os
 import copy
 from bs4 import BeautifulSoup
-#from hyphenate import hyphenate_word
 import csv
 import getopt
 import sys
-from nltk.corpus import wordnet as wn
 
+# Setup a dictionary of emotional data. Each key is a word and the value is a 
+# list of emotion values for each of the four emotions anger, fear joy sadness
+# in that order.
 temo = open("data/emo.txt").readlines()
 temo = [e.strip() for e in temo]
 temo = [e.split(",") for e in temo]
@@ -17,6 +18,7 @@ emo = {}
 for e in temo:
    emo[e[0]] = [float(e[1]),float(e[2]),float(e[3]),float(e[4])]
 
+# Setup a dictionary of frequency data. Keys = word, Values = # of occurences.
 tfreq = open("data/freq.txt").readlines()
 tfreq = [t.strip() for t in tfreq]
 tfreq = [t.split(" ") for t in tfreq]
@@ -26,6 +28,7 @@ for t in tfreq:
    t[1] = t[1].lower()
    freq[t[1]] = float(t[0])
 
+# Specify IN/OUT files. 
 ofile = open("data/out_test.txt","w")
 bs = BeautifulSoup(open("data/ham.xml").read())
 
@@ -33,11 +36,12 @@ bs = bs.find("body")
 act = "0"
 scene = "0"
 lineno = "0"
-
-
-same_line = False
 last_line = "-1"
 last_speaker = ""
+# Main loop for writing text to the out file. 
+# Notes on ham.xml:
+# - Acts and scenes are the outside div statements. Within each scene there are
+# - "head", "stage", and "sp" for headers, stage directions and speeches. 
 for acts in bs.findAll("div", recursive = False):
    for scenes in acts.findAll("div", recursive = False):
       for s in scenes.findAll(["head","stage","sp"], recursive = False):
@@ -49,44 +53,50 @@ for acts in bs.findAll("div", recursive = False):
             sp = g.string
             sp = sp.encode('utf-8')
             sp = re.sub(" ","_",sp)
-               
+            # Within each speech we can have "l"or "ab" lines of
+            # poetry or prose and "stage" in line stage directions. 
             for l in s.findAll(["l","ab","stage"]):
                scene_index += 1
+               # If the line is a stage direction we force the opening and  closing
+               # parens and add the stage directions.
                if l.name == "stage":
                   t = l.string.encode("utf-8","ignore")
                   wwhhaatt = l['type']
-              
+                  # Formatting substitutions.
                   t = re.sub("\s+"," ",t)
-              
                   t = re.sub("\."," .",t)
                   t = re.sub("\;"," ;",t)
                   t = re.sub("\:"," :",t)
                   t = re.sub("\,"," ,",t)
                   t = re.sub("\?"," ?",t)
                   t = re.sub("\!"," !",t)
-              
+                  # Open parens
                   ofile.write(act+" "+scene+" "+lineno+" -1 "+"Stage -1 -1 -1 -1 -1 ( (\n")
-              
+                  # Text of the stage direction
                   for w in t.split(" "):
               
                      ofile.write(act+" "+scene+" "+lineno+" -1 "+"Stage -1 -1 -1 -1 -1 "+wwhhaatt+" "+w+"\n")
-              
+                  # Close parens and NEWLINE.
                   ofile.write(act+" "+scene+" "+lineno+" -1 "+"Stage -1 -1 -1 -1 -1 ) )\n")
                   ofile.write(act+" "+scene+" "+lineno+" -1 "+"Stage -1 -1 -1 -1 -1 NEWLINE NEWLINE\n")
                   continue
-      
+               # If l is not a "stage then we want treat it as a line of text.
+               # Set the line number lineno from the "n" attribute in the line
                lineno = l["n"].strip()
                lineno = lineno.encode('utf-8','ignore')
                
-               if lineno == last_line:
-                  same_line = True
-               #if the sameline flag is set then we add the previous lineno
-               if not same_line:
+               # If it is a new line but with the same speaker 
+               # - we do not want to add a newline because it will be separated from the previous line 
+               #   by the newline of the previous line.
+               # If it is a new speaker regardless of if it is a new line.
+               # - we add the new speaker and a newline.
+               # If it is the same line but with  speaker
+               # - we want a newline but no new character 
+               if lineno != last_line:
                   tmp = int(last_line)
                   tmp += 1
                   tmp = str(tmp)
                   tmp = tmp.encode('utf-8','ignore')
-                  #if the sameline flag is set then we add the previous lineno
                   if last_speaker != sp:
                      ofile.write(act+" "+scene+" "+tmp+" -1 "+sp+" -1 -1 -1 -1 -1 SPEAKER "+sp.upper()+"\n")
                      ofile.write(act+" "+scene+" "+tmp+" -1 "+sp+" -1 -1 -1 -1 -1 NEWLINE NEWLINE\n")
@@ -96,48 +106,47 @@ for acts in bs.findAll("div", recursive = False):
                   else:
                      ofile.write(act+" "+scene+" "+lineno+" -1 "+sp+" -1 -1 -1 -1 -1 SPEAKER "+sp.upper()+"\n")
                      ofile.write(act+" "+scene+" "+lineno+" -1 "+sp+" -1 -1 -1 -1 -1 NEWLINE NEWLINE\n")
-                  same_line = False
-         
+               # Processing for each word in a line of text.
                for w in l.findAll("w"):
-      
+                  # Word number. 
                   wordno = w["ord"].strip()
                   wordno = wordno.encode('utf-8','ignore')
-        
+                  
+                  # The word string. 
                   ww = w.string.encode('utf-8','ignore')
                   ww = re.sub("\s+"," ",ww)
                   ww = ww.strip()
-      
+                  
                   tst = ww
                   tst = tst.lower()
                   tst = re.sub("(.*)\W+$","\\1",tst)
-      
+                  
+                  # If it is a word we add a string of the emotion values for 
+                  # the word. Otherwise we add the string of "-1"'s or "0"'s
                   if tst in emo:
-      
                      affs = " ".join([str(e) for e in emo[tst]])
                      fffs = str(freq[tst])
-      
                   elif not tst:
-      
                      affs = "-1 -1 -1 -1"
                      fffs = "-1"
-      
                   else:
-      
                      affs = "0 0 0 0"
                      fffs = "0"
-      
+                  # Write the new word to the out file. 
                   if ww: 
-      
                      ofile.write(act+" "+scene+" "+lineno+" "+wordno+" "+sp+" "+\
                               affs+" "+fffs+" "+w["pos"].encode("utf-8")+" "+ww+"\n")
-               last_line = lineno
+               # We don't want to add newline on "ab" lines. 
                if l.name != "ab" or scene_index == scene_length:
                   ofile.write(act+" "+scene+" "+lineno+" -1 "+sp+" -1 -1 -1 -1 -1 NEWLINE NEWLINE\n")
 
-               #last_line = lineno
+               # Reset variables.
                last_speaker = sp
-      
+               last_line = lineno
+               
          elif s.name == "stage":
+            # If the line is a stage direction we force the opening and  closing
+            # parens and add the stage directions.
             l = s.string.encode("utf-8","ignore")
             wwhhaatt = s['type']
          
