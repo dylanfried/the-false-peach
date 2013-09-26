3bacbade6523a6ca6780e617e28a4233e810cdc1
Dylan Fried <dylan.fried@gmail.com>
2013-09-26 14:34:20 +0200

    bumped up prose line length for comma splitting, put in max line length of 50

--- code/generator.py
+++ code/generator.py
@@ -687,11 +687,11 @@ class Generator:
          if "max_line_length" in current_chunk:
             max_line_length = current_chunk['max_line_length']
          else:
-            max_line_length = 17
+            max_line_length = 50
          if self.line_length > 0 and \
             ((next_word[-2] == "dumb" and (re.match(".*[.?!:;]\s*$",next_word[-1]) or self.line_length > 9 and re.match(".*[,]\s*$",next_word[-1]))) or \
-            (current_chunk["chunk_type"] != "mirror" and self.current_line_prose/(self.line_length+0.0)>=0.1 and ((self.line_length > 6 and re.match(".*[,]\s*$",next_word[-1])) or (re.match(".*[.?!:;]\s*$",next_word[-1])))) or \
-            (((self.line_length > 13 and re.match(".*[,]\s*$",next_word[-1])) or (self.line_length > 10 and re.match(".*[.?!:;]\s*$", next_word[-1]))))):
+            (current_chunk["chunk_type"] != "mirror" and self.current_line_prose/(self.line_length+0.0)>=0.1 and ((self.line_length > 8 and re.match(".*[,]\s*$",next_word[-1])) or (re.match(".*[.?!:;]\s*$",next_word[-1])))) or \
+            ((self.line_length > max_line_length or (self.line_length > 13 and re.match(".*[,]\s*$",next_word[-1])) or (self.line_length > 10 and re.match(".*[.?!:;]\s*$", next_word[-1]))))):
 
             if self.in_paren:
                insert_paren = [None]*13
